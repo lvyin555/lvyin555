4f6d937c2b626893fedbdeed9607ab7d5378e8f1
jwy2008 <1441346781@qq.com>
2022-05-06 21:23:06 +0800

    Add files via upload

---
+++ config/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,27 @@
+#Import-Module PSReadLineKeyHandler
+#Import-Module posh-git
+#Import-Module oh-my-posh
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadlineOption -EditMode Emacs
+Set-PSReadLineKeyHandler -Chord "RightArrow" -Function ForwardWord
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PoshPrompt bubbles
+Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
+oh-my-posh prompt init pwsh --config C:\Users\14413\AppData\Local\oh-my-posh\themes/jandedobbeleer.omp.json | Invoke-Expression
+function _sudo {
+    $ss = "$args ; pause"
+    Start-Process powershell -Verb runAs -ArgumentList $ss
+}
+set-alias  -name sudo -value _sudo
+Set-PSReadLineOption -Colors @{
+  Command            = 'Green'
+  Number             = 'Green'
+  Member             = 'Green'
+  Operator           = 'Yellow'
+  Type               = 'Green'
+  Variable           = 'Yellow'
+  Parameter          = 'Yellow'
+  ContinuationPrompt = 'Green'
+  Default            = 'Green'
+}
+cat C:\Users\14413\Documents\Powershell_out.txt
