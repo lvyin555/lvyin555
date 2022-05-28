09c69352f1e05489cf9f841073ad51dd82fe09d9
jwy2008 <1441346781@qq.com>
2022-05-28 10:19:51 +0800

    Update Microsoft.PowerShell_profile.ps1

--- config/wt/Microsoft.PowerShell_profile.ps1
+++ config/wt/Microsoft.PowerShell_profile.ps1
@@ -1,29 +1,16 @@
-#Import-Module PSReadLineKeyHandler
-#Import-Module posh-git
-#Import-Module oh-my-posh
+#Import-Module PSReadLineKeyHandler, posh-git, oh-my-posh
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadlineOption -EditMode Emacs
 Set-PSReadLineKeyHandler -Chord "RightArrow" -Function ForwardWord
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PoshPrompt bubbles
 Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
-oh-my-posh prompt init pwsh --config C:\Users\14413\AppData\Local\oh-my-posh\themes/jandedobbeleer.omp.json | Invoke-Expression
+oh-my-posh prompt init pwsh --config ~\Documents\PowerShell\themes\themes.json | Invoke-Expression
 function _sudo {
     $ss = "$args ; pause"
     Start-Process pwsh -Verb runAs -ArgumentList $ss
 }
 set-alias  -name sudo -value _sudo
-Set-PSReadLineOption -Colors @{
-  Command            = 'Green'
-  Number             = 'Green'
-  Member             = 'Green'
-  Operator           = 'Yellow'
-  Type               = 'Green'
-  Variable           = 'Yellow'
-  Parameter          = 'Yellow'
-  ContinuationPrompt = 'Green'
-  Default            = 'Green'
-}
 function getlist {Get-ChildItem -Name}
 function wslvim {
   $ss = "$args"
@@ -33,7 +20,12 @@ function wslzsh {
   $ss = "$args"
   wsl zsh $ss
 }
+function wslbash {
+  $ss = "$args"
+  wsl bash $ss
+}
 Set-Alias ls getlist
 Set-Alias zsh wslzsh
 Set-Alias vim wslvim
-cat C:\Users\14413\Documents\Powershell_out.txt
+Set-Alias bash wslbash
+Get-Content ~\Documents\PowerShell\Powershell_out.txt
