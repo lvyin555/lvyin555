c530789771b8564c6f43b1b63284baa5aa2092e3
律音 <1441346781@qq.com>
2023-09-23 23:17:53 +0800

    Update Microsoft.PowerShell_profile.ps1

--- config/wt/Microsoft.PowerShell_profile.ps1
+++ config/wt/Microsoft.PowerShell_profile.ps1
@@ -3,7 +3,7 @@ Set-PSReadLineOption -PredictionSource History
 Set-PSReadlineOption -EditMode Emacs
 Set-PSReadLineKeyHandler -Chord "RightArrow" -Function ForwardWord
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-Set-PoshPrompt bubbles
+#Set-PoshPrompt bubbles
 Set-PSReadLineKeyHandler -Key "Ctrl+z" -Function Undo
 oh-my-posh prompt init pwsh --config ~\Documents\PowerShell\themes\themes.json | Invoke-Expression
 function _sudo {
@@ -28,4 +28,4 @@ Set-Alias ls getlist
 Set-Alias zsh wslzsh
 Set-Alias vim wslvim
 Set-Alias bash wslbash
-Get-Content ~\Documents\PowerShell\Powershell_out.txt
+neofetch
